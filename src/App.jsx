de6baf40133d6e619f780a892680dbe7ebadac6c
jesuseunfunmi <jesuseunfunmi@users.noreply.github.com>
2024-09-17 17:43:41 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import {Link,Route,Routes} from "react-router-dom"
 import About from './component/About'
 import Home from './component/Home'
 
+// import server from './server/server'
+import Vans from './page/Vans'
+import VanDetail from './page/VanDetail'
+
 
 function App() {
 
@@ -11,11 +15,14 @@ function App() {
       <Link className='site-logo' to="/">#VANLIFE</Link>
       <nav>
         <Link to="/about">About</Link>
+        <Link to="/vans">Vans</Link>
       </nav>
     </header>
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
+      <Route path='/vans' element={<Vans/>}/>
+      <Route path='/vans/:id' element={<VanDetail/>}/>
      </Routes>
     </>
   )
